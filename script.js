b199fe9c948c2fd8a8181afae7440be927a40180
jahongiruz10 <ziyodullayevjahongir82@gmail.com>
2025-07-18 08:49:48 +0500

    dawafcaew
    fwqdfwed

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-async function tarjimaQil() {
-  const matn = document.querySelector(".matn").value;
-  const src = document.querySelector(".src-lang").value;
-  const dest = document.querySelector(".dest-lang").value;
-  const natija = document.getElementById("natija");
+let btn = document.querySelector('button');
 
-  if (!matn.trim()) {
-    natija.innerText = "Iltimos, matn kiriting!";
-    return;
-  }
-
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${src}&tl=${dest}&dt=t&q=${encodeURIComponent(matn)}`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    let tarjima = "";
-    data[0].forEach(qism => tarjima += qism[0]);
-    natija.innerText = tarjima;
-  } catch (err) {
-    natija.innerText = "Xatolik yuz berdi. Internetni tekshiring.";
-    console.error(err);
-  }
+if (!localStorage.getItem('count')) {
+    let count = 0;
+    localStorage.setItem('count', count);
+} else {
+    let count = localStorage.getItem('count')
+    btn.textContent = `${count}`;
 }
 
-document.querySelector(".btn").addEventListener("click", tarjimaQil);
+let count = localStorage.getItem('count');
+
+btn.addEventListener('click', () => {
+  count++;
+  btn.textContent = `${count}`;
+
+  localStorage.setItem('count', count);
+});
